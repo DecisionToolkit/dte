d39cfe7b9f5c4e65e44680f77f50560124e4dce6
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-04-12 16:30:08 +0200

    Added key definitions.

---
+++ src/keys.rs
@@ -0,0 +1,62 @@
+use crossterm::event;
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
+
+const PRESS: KeyEventKind = KeyEventKind::Press;
+const S_NONE: KeyEventState = KeyEventState::NONE;
+const M_NONE: KeyModifiers = KeyModifiers::NONE;
+const M_CTRL: KeyModifiers = KeyModifiers::CONTROL;
+const M_SHIFT: KeyModifiers = KeyModifiers::SHIFT;
+
+#[derive(Debug)]
+pub enum Key {
+  Backspace,
+  Delete,
+  Down,
+  End,
+  Home,
+  Left,
+  Right,
+  Tab,
+  Up,
+  ShiftEnd,
+  ShiftHome,
+  ShiftTab,
+  CtrlQ,
+  Char(char),
+}
+
+pub fn read_key() -> Key {
+  loop {
+    match event::read() {
+      Ok(event) => {
+        match event {
+          Event::Key(KeyEvent { code, modifiers, kind, state }) => match (code, modifiers, kind, state) {
+            (KeyCode::Left, M_NONE, PRESS, S_NONE) => return Key::Left,
+            (KeyCode::Right, M_NONE, PRESS, S_NONE) => return Key::Right,
+            (KeyCode::Up, M_NONE, PRESS, S_NONE) => return Key::Up,
+            (KeyCode::Down, M_NONE, PRESS, S_NONE) => return Key::Down,
+            (KeyCode::Backspace, M_NONE, PRESS, S_NONE) => return Key::Backspace,
+            (KeyCode::Delete, M_NONE, PRESS, S_NONE) => return Key::Delete,
+            (KeyCode::End, M_NONE, PRESS, S_NONE) => return Key::End,
+            (KeyCode::End, M_SHIFT, PRESS, S_NONE) => return Key::ShiftEnd,
+            (KeyCode::Home, M_NONE, PRESS, S_NONE) => return Key::Home,
+            (KeyCode::Home, M_SHIFT, PRESS, S_NONE) => return Key::ShiftHome,
+            (KeyCode::Tab, M_NONE, PRESS, S_NONE) => return Key::Tab,
+            (KeyCode::BackTab, M_SHIFT, PRESS, S_NONE) => return Key::ShiftTab,
+            (KeyCode::Char('q'), M_CTRL, PRESS, S_NONE) => return Key::CtrlQ,
+            (KeyCode::Char(ch), _, PRESS, S_NONE) => return Key::Char(ch),
+            _ => {
+              // TODO remove
+              println!("{:?}", event);
+            }
+          },
+          _ => {
+            // TODO remove
+            println!("{:?}", event);
+          }
+        }
+      }
+      _ => {}
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Decision table editor");
+mod keys;
+
+use crate::keys::{read_key, Key};
+use crossterm::{event, terminal, ExecutableCommand};
+use std::fmt::Display;
+use std::{fmt, io};
+
+fn run() -> io::Result<()> {
+  let mut stdout = io::stdout();
+  stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+  loop {
+    match read_key() {
+      Key::CtrlQ => break,
+      other => {
+        // TODO remove
+        println!("{:?}", other)
+      }
+    }
+  }
+  Ok(())
+}
+
+fn main() -> io::Result<()> {
+  // enter raw mode, directly process each key press
+  terminal::enable_raw_mode()?;
+  let _ = run();
+  // return to canonical mode, process input after pressing Enter
+  terminal::disable_raw_mode()?;
+  Ok(())
 }
