930083da13faf6d268d6bc0c5c59c0aa21b0a2aa
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-04-16 17:18:54 +0200

    Updates.

---
+++ editor/src/editor.rs
@@ -0,0 +1,164 @@
+use crate::keys::{read_key, Key};
+use crossterm::cursor::{MoveTo, SetCursorStyle};
+use crossterm::style::Print;
+use crossterm::{execute, queue, terminal};
+use dtee::Controller;
+use std::io;
+use std::io::{Stdout, Write};
+
+pub struct Editor {
+  stdout: Stdout,
+  controller: Controller,
+}
+
+impl Editor {
+  /// Creates a new editor initialized with specified text.
+  pub fn new(text: String) -> io::Result<Self> {
+    let stdout = io::stdout();
+    let (columns, rows) = terminal::size()?;
+    let controller = Controller::new(text, columns as usize, rows as usize);
+    Ok(Self { stdout, controller })
+  }
+
+  /// Starts text editing loop.
+  pub fn run(&mut self) -> io::Result<()> {
+    execute!(self.stdout, terminal::EnterAlternateScreen)?;
+    execute!(self.stdout, terminal::Clear(terminal::ClearType::All))?;
+    self.repaint()?;
+    execute!(self.stdout, SetCursorStyle::BlinkingBar, MoveTo(1, 1))?;
+    loop {
+      match read_key() {
+        Key::Right => self.action_cursor_move_right()?,
+        Key::Left => self.action_cursor_move_left()?,
+        Key::Up => self.action_cursor_move_up()?,
+        Key::Down => self.action_cursor_move_down()?,
+        Key::Home => self.action_cursor_move_cell_start()?,
+        Key::ShiftHome => self.action_cursor_move_row_start()?,
+        Key::End => self.action_cursor_move_cell_end()?,
+        Key::ShiftEnd => self.action_cursor_move_row_end()?,
+        Key::Tab => self.action_cursor_move_cell_next()?,
+        Key::ShiftTab => self.action_cursor_move_cell_prev()?,
+        Key::Insert => self.action_cursor_toggle_bar_block()?,
+        Key::Char(ch) => self.action_write(ch)?,
+        Key::Resize(width, height) => self.action_resize(width, height)?,
+        Key::CtrlQ => break,
+        _ => {}
+      };
+    }
+    execute!(self.stdout, terminal::LeaveAlternateScreen)?;
+    Ok(())
+  }
+
+  fn action_cursor_move_right(&mut self) -> io::Result<()> {
+    if let Some((column, row)) = self.controller.cursor_move_right() {
+      execute!(self.stdout, MoveTo(column as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_left(&mut self) -> io::Result<()> {
+    if let Some((column, row)) = self.controller.cursor_move_left() {
+      execute!(self.stdout, MoveTo(column as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_up(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_up() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_down(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_down() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_cell_start(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_cell_start() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_cell_end(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_cell_end() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_row_start(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_row_start() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_row_end(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_row_end() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_cell_next(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_cell_next() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_move_cell_prev(&mut self) -> io::Result<()> {
+    if let Some((col, row)) = self.controller.cursor_move_cell_prev() {
+      execute!(self.stdout, MoveTo(col as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_cursor_toggle_bar_block(&mut self) -> io::Result<()> {
+    self.controller.cursor_toggle_bar_block();
+    if self.controller.cursor_is_bar() {
+      execute!(self.stdout, SetCursorStyle::BlinkingBar)?;
+    }
+    if self.controller.cursor_is_block() {
+      execute!(self.stdout, SetCursorStyle::BlinkingBlock)?;
+    }
+    // refresh the character under the cursor that has changed
+    if let Some(ch) = self.controller.cursor_char() {
+      let (column, row) = self.controller.cursor_position();
+      execute!(self.stdout, Print(ch), MoveTo(column as u16, row as u16))?;
+    }
+    Ok(())
+  }
+
+  fn action_resize(&mut self, width: usize, height: usize) -> io::Result<()> {
+    self.controller.resize(width, height);
+    self.repaint()?;
+    let (column, row) = self.controller.cursor_position();
+    execute!(self.stdout, MoveTo(column as u16, row as u16))?;
+    Ok(())
+  }
+
+  fn action_write(&mut self, _ch: char) -> io::Result<()> {
+    // execute!(self.stdout, Print(ch))?,
+    Ok(())
+  }
+
+  /// Refreshes dirty regions.
+  fn repaint(&mut self) -> io::Result<()> {
+    for rect in self.controller.dirty() {
+      let top = rect.top();
+      let left = rect.left();
+      for (r, row) in self.controller.text().iter().skip(top).take(rect.height()).enumerate() {
+        for (c, ch) in row.iter().skip(left).take(rect.width()).enumerate() {
+          let _ = queue!(self.stdout, MoveTo((left + c) as u16, (top + r) as u16), Print(ch));
+        }
+      }
+    }
+    self.stdout.flush()
+  }
+}
--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,121 +1,16 @@
+mod editor;
 mod keys;
 
-use crate::keys::{read_key, Key};
-use crossterm::cursor::{MoveTo, SetCursorStyle};
-use crossterm::style::Print;
-use crossterm::terminal::{size, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::{execute, queue, terminal};
-use dtee::{Controller, Rect};
-use std::io::{Stdout, Write};
+use crate::editor::Editor;
+use crossterm::terminal;
 use std::{fs, io};
-
-fn repaint(stdout: &mut Stdout, content: &[Vec<char>], dirty: &[Rect]) -> io::Result<()> {
-  for rect in dirty {
-    let top = rect.top();
-    let left = rect.left();
-    for (r, row) in content.iter().skip(top).take(rect.height()).enumerate() {
-      for (c, ch) in row.iter().skip(left).take(rect.width()).enumerate() {
-        let _ = queue!(stdout, MoveTo((left + c) as u16, (top + r) as u16), Print(ch));
-      }
-    }
-  }
-  stdout.flush()
-}
-
-fn run(content: String) -> io::Result<()> {
-  let mut stdout = io::stdout();
-  execute!(io::stdout(), EnterAlternateScreen)?;
-  execute!(stdout, Clear(ClearType::All))?;
-  let (cols, rows) = size()?;
-  let mut controller = Controller::new(content, cols as usize, rows as usize);
-  repaint(&mut stdout, controller.text(), controller.dirty())?;
-  execute!(stdout, SetCursorStyle::BlinkingBar, MoveTo(1, 1))?;
-  loop {
-    match read_key() {
-      Key::Right => {
-        if let Some((col, row)) = controller.cursor_move_right() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::Left => {
-        if let Some((col, row)) = controller.cursor_move_left() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::Up => {
-        if let Some((col, row)) = controller.cursor_move_up() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::Down => {
-        if let Some((col, row)) = controller.cursor_move_down() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::Home => {
-        if let Some((col, row)) = controller.cursor_move_cell_start() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::ShiftHome => {
-        if let Some((col, row)) = controller.cursor_move_row_start() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::End => {
-        if let Some((col, row)) = controller.cursor_move_cell_end() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::ShiftEnd => {
-        if let Some((col, row)) = controller.cursor_move_row_end() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::Tab => {
-        if let Some((col, row)) = controller.cursor_move_cell_next() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::ShiftTab => {
-        if let Some((col, row)) = controller.cursor_move_cell_prev() {
-          execute!(stdout, MoveTo(col as u16, row as u16))?;
-        }
-      }
-      Key::CtrlQ => break,
-      Key::Insert => {
-        controller.cursor_toggle_bar_block();
-        if controller.cursor_is_bar() {
-          execute!(stdout, SetCursorStyle::BlinkingBar)?;
-        }
-        if controller.cursor_is_block() {
-          execute!(stdout, SetCursorStyle::BlinkingBlock)?;
-        }
-        // reprint the character under the cursor that has changed
-        if let Some(ch) = controller.cursor_char() {
-          let (column, row) = controller.cursor_position();
-          execute!(stdout, Print(ch), MoveTo(column as u16, row as u16))?;
-        }
-      }
-      Key::Char(ch) => execute!(stdout, Print(ch))?,
-      Key::Resize(width, height) => {
-        controller.resize(width, height);
-        repaint(&mut stdout, controller.text(), controller.dirty())?;
-        let (column, row) = controller.cursor_position();
-        execute!(stdout, MoveTo(column as u16, row as u16))?;
-      }
-      _ => {}
-    };
-  }
-  execute!(stdout, Clear(ClearType::All), SetCursorStyle::DefaultUserShape, MoveTo(0, 0))?;
-  execute!(io::stdout(), LeaveAlternateScreen)?;
-  Ok(())
-}
 
 fn main() -> io::Result<()> {
   terminal::enable_raw_mode()?;
   let content = fs::read_to_string("./examples/e2.dtb").expect("Failed to load file");
-  let _ = run(content);
+  if let Ok(mut editor) = Editor::new(content) {
+    let _ = editor.run();
+  }
   terminal::disable_raw_mode()?;
   Ok(())
 }
