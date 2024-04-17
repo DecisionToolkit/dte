ca1466bf8170321139ff87998caa3a1dc5327c96
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-04-17 19:30:00 +0200

    Prepared version 0.0.1

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -3,18 +3,28 @@ mod keys;
 mod utils;
 
 use crate::editor::Editor;
+use clap::{arg, command, ArgMatches};
 use crossterm::terminal;
-use std::fs::read_to_string;
+use std::fs;
 use std::io::Result;
+
+fn get_matches() -> ArgMatches {
+  command!()
+    .arg(arg!(<INPUT_FILE>).help("File containing decision table to edit").required(true).index(1))
+    .get_matches()
+}
 
 fn start(content: String) -> Result<()> {
   Editor::new(content)?.start()
 }
 
 fn main() -> Result<()> {
-  let content = read_to_string("./examples/e2.dtb").expect("Failed to load file");
-  terminal::enable_raw_mode()?;
-  let _ = start(content);
-  terminal::disable_raw_mode()?;
+  let matches = get_matches();
+  let file_name = matches.get_one::<String>("INPUT_FILE").unwrap().to_string();
+  if let Ok(content) = fs::read_to_string(file_name) {
+    terminal::enable_raw_mode()?;
+    let _ = start(content);
+    terminal::disable_raw_mode()?;
+  }
   Ok(())
 }
