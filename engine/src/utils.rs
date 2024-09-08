5733ed8f511036738fa455f134a2860de55a732c
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-09-08 17:41:59 +0200

    Updates.

--- engine/src/utils.rs
+++ engine/src/utils.rs
@@ -4,7 +4,13 @@ use std::io::Write;
 const FILE_NAME: &str = "./target/output.txt";
 
 pub fn debug_to_file(message: impl ToString) {
-  let mut file = OpenOptions::new().create(true).append(true).open(FILE_NAME).expect("failed to open debug output file");
-  let _ = file.write(format!("{}\n", message.to_string()).as_bytes()).expect("failed to write debug message");
-  file.flush().expect("failed to flush debug output file");
+  let mut file = OpenOptions::new()
+    .create(true)
+    .append(true)
+    .open(FILE_NAME)
+    .unwrap_or_else(|_| panic!("failed to open debug file: {}", FILE_NAME));
+  let _ = file
+    .write(format!("{}\n", message.to_string()).as_bytes())
+    .unwrap_or_else(|_| panic!("failed to write debug message: {}", FILE_NAME));
+  file.flush().unwrap_or_else(|_| panic!("failed to flush debug file: {}", FILE_NAME));
 }
